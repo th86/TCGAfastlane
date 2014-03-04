5ba169fdd0e7fff591de78d9b89a1792a331828b
= <=>
2014-03-04 09:36:19 -0500

    descriptions

--- rsem2rda.R
+++ rsem2rda.R
@@ -1,54 +1,20 @@
-#TCGA Data Processing Pipeline for R
-#Tai-Hsien Ou Yang
-#Columbia University
+
+#' TCGA Fastlane
+#' 
+#' TCGA Data Processing Pipeline for R
+#' Columbia University GISL
+#' @author Tai-Hsien Ou Yang
+#' @export 
 
 
 
-
-DATA_PATH="C:\\workspace\\TCGA_RSEM\\KIRC\\unc.edu_KIRC.IlluminaHiSeq_RNASeqV2.Level_3.1.3.0\\"
-SDRF_FILE="C:\\workspace\\TCGA_RSEM\\KIRC\\unc.edu_KIRC.IlluminaHiSeq_RNASeqV2.1.3.0.sdrf.txt"
-SAMPLE_TYPE_FILE="C:\\workspace\\TCGA_RSEM\\KIRC\\nationwidechildrens.org_biospecimen_sample_kirc.txt"
+DATA_PATH="/media/taihsien/F620B94320B90C1D/dataset/TCGA/KIRC/unc.edu_KIRC.IlluminaHiSeq_RNASeqV2.Level_3.1.3.0/"
+SDRF_FILE="/media/taihsien/F620B94320B90C1D/dataset/TCGA/KIRC/unc.edu_KIRC.IlluminaHiSeq_RNASeqV2.1.3.0.sdrf.txt"
+SAMPLE_TYPE_FILE="/media/taihsien/F620B94320B90C1D/dataset/TCGA/KIRC/nationwidechildrens.org_biospecimen_sample_kirc.txt"
 SAMPLE_TYPE="Primary Tumor"
 
 
-rsem2rda<-function( DATA_PATH, SDRF_FILE, SAMPLE_TYPE_FILE,SAMPLE_TYPE  , OUTPUT_FILE="e.rda"   ){
-
-    files = dir(path= DATA_PATH, full.names=TRUE);
-
-    #Read the RSEM data 
-    files = files[grep(".rsem.genes.results", files)];
-    data <- lapply(files, read.delim, stringsAsFactors=FALSE, as.is=TRUE);
-
-    #Data filtering
-    files<-gsub(".*unc.edu.","",files)
-    files<-gsub(".rsem.genes.results","",files)
-    files<-substr(files,1,36)
-
-    #Read the SDRF data
-    sdrf <- read.delim(SDRF_FILE, as.is=TRUE);
-    #https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/brca/cgcc/unc.edu/illuminahiseq_rnaseqv2/rnaseqv2/unc.edu_BRCA.IlluminaHiSeq_RNASeqV2.mage-tab.1.7.0/
-
-
-#Read the sample information data
-sample_type<-read.delim(  SAMPLE_TYPE_FILE,as.is=TRUE  )
-
-sample_retain_barcode= as.character(sample_type[ which(  sample_type[, "sample_type" ]== SAMPLE_TYPE), "bcr_sample_barcode" ])
-sample_retain_uuid= as.character(sample_type[ which(  sample_type[, "sample_type" ]== SAMPLE_TYPE), "bcr_sample_uuid" ])
-
-
-
-
-
-
-rownames(sdrf)<-make.unique(sdrf[,1])
-barcodes <- sdrf[files, 2]
-#  combine as matrix
-
-data <- lapply(1:length(data), function(i) cbind(barcode=barcodes[i],
-      data[[i]]))
-rdata <- do.call(rbind, data );
-
-
+#Extract gene symbols
 getGeneSymbols = function(innames){
     outnames = sapply(innames, function(x){
     
@@ -63,34 +29,85 @@ getGeneSymbols = function(innames){
     )
 }
 
+#rsem2rda( DATA_PATH, SDRF_FILE, SAMPLE_TYPE_FILE,SAMPLE_TYPE  , OUTPUT_FILE="e.rda"   )
+
+#Convert genomic profiles into a matrix
+rsem2rda<-function( DATA_PATH=DATA_PATH, SDRF_FILE=SDRF_FILE, SAMPLE_TYPE_FILE=SAMPLE_TYPE_FILE,SAMPLE_TYPE=SAMPLE_TYPE  , OUTPUT_FILE="e.rda"   ){
+
+#Read the RSEM data 
+cat("Sorting genomic profiles...")
+files = dir(path= DATA_PATH, full.names=TRUE);
+files = files[grep(".rsem.genes.results", files)];
+data <- lapply(files, read.delim, stringsAsFactors=FALSE, as.is=TRUE);
+
+#Data filtering
+files<-gsub(".*unc.edu.","",files)
+files<-gsub(".rsem.genes.results","",files)
+files<-substr(files,1,36)
+
+#Read the SDRF data
+sdrf <- read.delim(SDRF_FILE, as.is=TRUE);
+#https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/brca/cgcc/unc.edu/illuminahiseq_rnaseqv2/rnaseqv2/unc.edu_BRCA.IlluminaHiSeq_RNASeqV2.mage-tab.1.7.0/
+cat("DONE\n")
+
+#Read the sample information data
+cat("Parsing sample information...")
+sample_type<-read.delim(  SAMPLE_TYPE_FILE,as.is=TRUE  )
+
+sample_retain_barcode= as.character(sample_type[ which(  sample_type[, "sample_type" ]== SAMPLE_TYPE), "bcr_sample_barcode" ])
+sample_retain_uuid= as.character(sample_type[ which(  sample_type[, "sample_type" ]== SAMPLE_TYPE), "bcr_sample_uuid" ])
+cat("DONE\n")
+
+rownames(sdrf)<-make.unique(sdrf[,1])
+barcodes <- sdrf[files, 2]
+
+#Combine as matrix
+cat("Parsing genomic profiles...")
+data <- lapply(1:length(data), function(i) cbind(barcode=barcodes[i],
+      data[[i]]))
+rdata <- do.call(rbind, data );
+cat("DONE\n")
 
 
-
+#Initiate a matrix
+cat("Merging...")
 geneName<-unique(rdata[,"gene_id"])
 barcode<-unique(rdata[,"barcode"])
 
 e<-matrix(0, length(geneName), length(barcode) )
 rownames(e)<-geneName
 colnames(e)<-barcode
+rownames(e)<-getGeneSymbols(geneName)
+colnames(e)<-substr(colnames(e),1,16)
 
+
+#Conversion
+b <- txtProgressBar(style=3)
 for( i in 1:length(barcode)){
     if(is.na(barcode[i])==FALSE){
         sample_e<- rdata[ which( rdata[,"barcode"]==barcode[i]),3]
         e[,i]<-sample_e
     }
-    cat(i, "processed\n")
-}
 
 
-rownames(e)<-getGeneSymbols(geneName)
-colnames(e)<-substr(colnames(e),1,16)
+    if(i %% 100 == 0)
+      setTxtProgressBar(b, i/length(barcode))    
+} #End of for 
+
+cat( length(barcode), " genomic profiles were parsed\n"   ) 
 
 
+
+#subset
 if( SAMPLE_TYPE !="" )
     e<-e[ , intersect( sample_retain_barcode , colnames(e) )]
 
+
+cat("Saving the matrix...")
 save(e,file=OUTPUT_FILE)
+cat("DONE\n")
 
 
+    return(1)
 }
 
